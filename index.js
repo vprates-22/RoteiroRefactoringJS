fede5234efef56925deeef1670baa03b0658f75f
vprates-ufmg <vprates2222@gmail.com>
2023-06-12 20:34:22 -0300

    Commit 2 - Extração de Função

--- index.js
+++ index.js
@@ -9,27 +9,32 @@ function gerarFaturaStr (fatura, pecas) {
                             minimumFractionDigits: 2 }).format;
   
     for (let apre of fatura.apresentacoes) {
-      const peca = pecas[apre.id];
-      let total = 0;
-  
-      switch (peca.tipo) {
-      case "tragedia":
-        total = 40000;
-        if (apre.audiencia > 30) {
-          total += 1000 * (apre.audiencia - 30);
+      
+      function calcularTotalApresentacao(apre, peca){
+        let total = 0;
+        switch (peca.tipo) {
+        case "tragedia":
+          total = 40000;
+          if (apre.audiencia > 30) {
+            total += 1000 * (apre.audiencia - 30);
+          }
+          break;
+        case "comedia":
+          total = 30000;
+          if (apre.audiencia > 20) {
+            total += 10000 + 500 * (apre.audiencia - 20);
+          }
+          total += 300 * apre.audiencia;
+          break;
+        default:
+            throw new Error(`Peça desconhecia: ${peca.tipo}`);al += 10000 + 500 * (apre.audiencia - 20);
         }
-        break;
-      case "comedia":
-        total = 30000;
-        if (apre.audiencia > 20) {
-           total += 10000 + 500 * (apre.audiencia - 20);
-        }
-        total += 300 * apre.audiencia;
-        break;
-      default:
-          throw new Error(`Peça desconhecia: ${peca.tipo}`);
+        return total;
       }
   
+      const peca = pecas[apre.id];
+      let total = calcularTotalApresentacao(apre, peca)
+
       // créditos para próximas contratações
       creditos += Math.max(apre.audiencia - 30, 0);
       if (peca.tipo === "comedia") 
