abc1f565ec502e629bffb99d1dd19fbc789d32c4
vprates-ufmg <vprates2222@gmail.com>
2023-06-12 21:27:27 -0300

    Commit 10 - Criando Arquivos

---
+++ repositorio.js
@@ -0,0 +1,12 @@
+// arquivo repositorio.js
+const { readFileSync } = require("fs");
+
+module.exports = class Repositorio {
+  constructor() {
+    this.pecas = JSON.parse(readFileSync("./pecas.json"));
+  }
+
+  getPeca(apre) {
+    return this.pecas[apre.id];
+  }
+};
