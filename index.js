d8624f3cfa353bd64353c9256de73584983e019a
vprates-ufmg <vprates2222@gmail.com>
2023-06-12 21:22:28 -0300

    Commit 9 - Classe Repositorio

--- index.js
+++ index.js
@@ -8,30 +8,40 @@ function formatarMoeda(valor) {
   }).format(valor / 100);
 }
 
-function getPeca(apresentacao, pecas) {
-  return pecas[apresentacao.id];
+class Repositorio {
+  constructor() {
+    this.pecas = JSON.parse(readFileSync("./pecas.json"));
+  }
+
+  getPeca(apre) {
+    return this.pecas[apre.id];
+  }
 }
 
 class ServicoCalculoFatura {
-  calcularCredito(apre, pecas) {
+  constructor(repo) {
+    this.repo = repo;
+  }
+
+  calcularCredito(apre) {
     let creditos = 0;
     creditos += Math.max(apre.audiencia - 30, 0);
-    if (getPeca(apre, pecas).tipo === "comedia")
+    if (this.repo.getPeca(apre).tipo === "comedia")
       creditos += Math.floor(apre.audiencia / 5);
     return creditos;
   }
 
-  calcularTotalCreditos(apresentacoes, pecas) {
+  calcularTotalCreditos(apresentacoes) {
     let creditos = 0;
     for (let apre of apresentacoes) {
-      creditos += this.calcularCredito(apre, pecas);
+      creditos += this.calcularCredito(apre);
     }
     return creditos;
   }
 
-  calcularTotalApresentacao(apre, pecas) {
+  calcularTotalApresentacao(apre) {
     let total = 0;
-    switch (getPeca(apre, pecas).tipo) {
+    switch (this.repo.getPeca(apre).tipo) {
       case "tragedia":
         total = 40000;
         if (apre.audiencia > 30) {
@@ -46,35 +56,33 @@ class ServicoCalculoFatura {
         total += 300 * apre.audiencia;
         break;
       default:
-        throw new Error(`Peça desconhecia: ${getPeca(apre, pecas).tipo}`);
+        throw new Error(`Peça desconhecia: ${this.repo.getPeca(apre).tipo}`);
     }
     return total;
   }
 
-  calcularTotalFatura(apresentacoes, pecas) {
+  calcularTotalFatura(apresentacoes) {
     let totalFatura = 0;
     for (let apre of apresentacoes) {
-      let total = this.calcularTotalApresentacao(apre, pecas);
+      let total = this.calcularTotalApresentacao(apre);
       totalFatura += total;
     }
     return totalFatura;
   }
 }
 
-function gerarFaturaStr(fatura, pecas, calc) {
+function gerarFaturaStr(fatura, calc) {
   let faturaStr = `Fatura ${fatura.cliente}\n`;
   for (let apre of fatura.apresentacoes) {
-    faturaStr += `  ${getPeca(apre, pecas).nome}: ${formatarMoeda(
-      calc.calcularTotalApresentacao(apre, pecas)
+    faturaStr += `  ${calc.repo.getPeca(apre).nome}: ${formatarMoeda(
+      calc.calcularTotalApresentacao(apre)
     )} (${apre.audiencia} assentos)\n`;
   }
   faturaStr += `Valor total: ${calc.calcularTotalFatura(
-    fatura.apresentacoes,
-    pecas
+    fatura.apresentacoes
   )}\n`;
   faturaStr += `Créditos acumulados: ${calc.calcularTotalCreditos(
-    fatura.apresentacoes,
-    pecas
+    fatura.apresentacoes
   )} \n`;
   return faturaStr;
 }
@@ -102,9 +110,8 @@ function gerarFaturaStr(fatura, pecas, calc) {
 // }
 
 const faturas = JSON.parse(readFileSync("./faturas.json"));
-const pecas = JSON.parse(readFileSync("./pecas.json"));
-const calc = new ServicoCalculoFatura();
-let faturaStr = gerarFaturaStr(faturas, pecas, calc);
+const calc = new ServicoCalculoFatura(new Repositorio());
+let faturaStr = gerarFaturaStr(faturas, calc);
 console.log(faturaStr);
 // faturaStr = gerarFaturaHTML(faturas, pecas, calc);
 // console.log(faturaStr);
